d4afc56e3d20f57df92066f9059dde5bf62b806f
Thiago Arthur <thiagodevprof@gmail.com>
2025-02-04 18:00:17 -0300

    Final Commit

--- final_project/index.js
+++ final_project/index.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
-
 const app = express();
 
 app.use(express.json());
@@ -11,7 +10,11 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    if(req.session.user){
+        next();
+    }else{
+        res.status(401).json({message:"Unauthorized"});
+    }
 });
  
 const PORT =5000;
